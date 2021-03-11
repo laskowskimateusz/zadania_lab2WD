14989d1ea1009aebb0681d61feb5b5d3781931a3
selfwezmir <self.wezmir@gmail.com>
2021-03-11 14:26:24 +0100

    Add Zad6.py

---
+++ Zad6.py
@@ -0,0 +1,10 @@
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+
+if (a > b) & (a > c):
+    print("Największa jest 'a'.")
+if (b > a) & (b > c):
+    print("Największa jest 'b'.")
+if (c > a) & (c > b):
+    print("Największa jest 'c'.")
