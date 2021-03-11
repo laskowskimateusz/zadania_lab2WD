86da0bfa6ae9ca241e78ad334fb742e22e9c7eab
selfwezmir <self.wezmir@gmail.com>
2021-03-11 14:26:02 +0100

    Add Zad3.py

---
+++ Zad3.py
@@ -0,0 +1,3 @@
+slownik_ulubione_gry = {1: "Counter Strike", 2: "Gothic 2 Noc Kruka", 3: "Wiedzmin 3", 4: "Mount&Blade Warband"}
+
+print("Liczba elementów w słowniku: ", len(slownik_ulubione_gry.keys()))
