eff8dde3dc3550e0b3d0355483cc26a521f06cb7
selfwezmir <self.wezmir@gmail.com>
2021-03-11 14:25:51 +0100

    Add Zad1.py

---
+++ Zad1.py
@@ -0,0 +1,8 @@
+lista_sportow = ['piłka nożna', 'siatkówka', 'tenis stołowy', 'szachy']
+print(lista_sportow)
+
+lista_sportow.reverse()
+print(lista_sportow)
+
+lista_sportow.extend(['koszykówka', 'bilard', 'dart'])
+print(lista_sportow)
